76f9633f7fd003f3b998c9b9301ea1b07da83738
kolibrie-eric <33067569+kolibrie-eric@users.noreply.github.com>
2022-06-30 21:13:19 +0200

    All lessons completed

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Socks'
+            cart: [],
+            premium: true
         }
-    }
+    },
+    methods: {
+        updateCart(id) {
+            this.cart.push(id)
+        }
+    },
 })
